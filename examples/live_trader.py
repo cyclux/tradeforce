62ce4ef5446204ed143159f2c0a46b4d98cb01da
cyclux <nevermind@cyclux.de>
2023-03-03 14:28:47 +0100

    new live_trading example

--- examples/live_trader.py
+++ examples/live_trader.py
@@ -5,8 +5,8 @@ from tradeforce import TradingEngine
 
 config = {
     "trader": {
-        "id": 1,
-        "creds_path": "exchange_creds.cfg",
+        "id": 3,
+        # "creds_path": "exchange_creds.cfg",
         "budget": 0,
         "maker_fee": 0.10,
         "taker_fee": 0.20,
@@ -16,28 +16,29 @@ config = {
             "buy_opportunity_factor": 0.10,
             "buy_opportunity_boundary": 0.05,
             "prefer_performance": 1,
-            "hold_time_limit": 12000,
-            "profit_factor": 1.70,
-            "profit_ratio_limit": 1,
+            "hold_time_limit": 1000,
+            "profit_factor": 1.10,
+            "profit_ratio_limit": 1.01,
             "window": 160,
         },
     },
     "market_history": {
         "candle_interval": "5min",
-        "history_timeframe": "60days",
+        "history_timeframe": "720days",
         "base_currency": "USD",
         "exchange": "bitfinex",
-        "load_history_via": "feather",
+        "load_history_via": "API",
         "check_db_consistency": True,
         "dump_to_feather": True,
         "backend": "mongodb",
         "backend_host": "localhost:1234",
-        "mongo_collection": "bfx_history_test",
+        "mongo_collection": "bfx_history_2y",
         "update_history": False,
-        "run_exchange_api": True,
+        "run_exchange_api": False,
         "keep_updated": True,
     },
 }
 
-assets = []  # ["BTC", "ETH", "XMR"]
-TradingEngine(config, assets).run()
+# To provide a subset, TradingEngine can also receive the optional argument "assets".
+# This has to be a list of asset symbols. For example: assets = ["BTC", "ETH", "XRP"]
+TradingEngine(config).run()
