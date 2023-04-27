4e55fa5b677360c4f472b59dcf5af51d0e83e8e4
cyclux <nevermind@cyclux.de>
2023-04-27 21:13:43 +0000

    Fix line ending: git auto LF

---
+++ examples/tradeforce_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$RUN_JUPYTERLAB" = "True" ]; then
+  pip install jupyterlab
+  pip install ipywidgets nbformat matplotlib plotly
+  jupyter lab --ip 0.0.0.0 --port 8889 --notebook-dir=/home/tf_docker/user_code
+elif [ "$RUN_MARKET_SERVER" = "True" ]; then
+  python dedicated_market_server.py
+else
+  python hyperparam_search_multiprocess.py
+fi
